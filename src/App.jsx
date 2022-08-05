10432c30d1fa498dcbb975bfe70af077043adf92
SAMUEL MARCIANO <samuel.marciano@senacsp.edu.br>
2022-08-04 22:13:11 -0300

    novo login

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Router }  from 'react-router-dom';
+
+import history from './services/history';
+import Routes from './routes/privateroutes';
+
+import { AuthProvider } from './Context/AuthContext';
+import { NavBar } from './components/UI/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <div>
+      <AuthProvider>
+        <Router history={history}>
+            <Routes />
+        </Router>
+      </AuthProvider>
+    </div>
+  )
+}
+
+export default App
---
+++ src/page/Usuarios/ListaUsuarios.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from 'react-router-dom';
+import api from "../../services/api";
+import { Context } from "../../Context/AuthContext";
+import { Table, Container, Button, Alert } from 'react-bootstrap';
+import { NavBar } from '../../components/UI/NavBar/NavBar'
+import './styles.css'
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css'
+
+export const ListaUsuarios = () =>{
+    const { authenticated, handleLogout} = useContext(Context);
+
+    const [data, setData] = useState([]);
+
+    const [status, setStatus] = useState({
+        type:'',
+        mensagem:'',
+        loading: true
+    });
+
+    const ConfirmDelete = (user) => {
+       
+    
+        confirmAlert({
+          title: "Deseja Excluir o usuario",
+          message:
+            "tem certeza que deja excluir o  " +
+            user.name +
+            "?",
+          buttons: [
+            {
+              label: "Sim",
+              onClick: () => handleDelete(user.id)
+            },
+            {
+              label: "Não",
+              onClick: () => getUsers()
+            }
+          ]
+        });
+      };
+    const handleDelete = async (idUser) =>{
+        const headers = {
+            'headers': {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+            },
+        }
+        await api.delete("/user/"+idUser, headers)
+        .then((response) =>{
+            setStatus({
+                type:'sucess',
+                mensagem:response.data.mensagem
+            })
+            getUsers()
+
+        }).catch((err) =>{
+            if(err.response){
+                setStatus({
+                    type:'error',
+                    mensagem: err.response.data.mensagem
+                })
+            } else {
+                setStatus({
+                    type:'error',
+                    mensagem: 'Erro: Tente mais tarde!'
+                })
+            }
+        })
+
+    }
+
+
+    const getUsers = async () =>{
+
+        const headers = {
+            'headers': {
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
+
+            },
+        }   
+        
+        await api.get("/users", headers)
+        .then((response) =>{
+            setData(response.data.users);
+            setStatus({loading:false})
+        }).catch((err) =>{
+            if(err.response){
+                setStatus({
+                    type:'error',
+                    mensagem: err.response.data.mensagem
+                })
+            } else {
+                setStatus({
+                    type:'error',
+                    mensagem: 'Erro: Tente mais tarde!'
+                })
+            }
+        })
+    }
+
+    useEffect( () =>{
+        getUsers();
+    }, [])
+
+
+    return(
+        <>
+            <NavBar />                    
+            <Container>
+            {status.loading ? <Alert variant="warning">Carregando...</Alert> : ""}
+            <div className="btnNovo">
+            <h1>Usuários</h1>
+            <Button variant="outline-success">
+                <Link className="noLink" to="/usuarios/novo">Novo Usuário</Link>
+            </Button>
+            </div>
+            <Table striped bordered hover>  
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>E-mail</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            {(!status.loading &&
+                data.map(user =>(
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td className="spaceFlex">
+                        <Button variant="outline-warning">
+                            <Link className="noLink " to={"/usuarios/editar/"+user.id}>Editar</Link>
+                        </Button>
+                        <Button variant="outline-danger" onClick={() => ConfirmDelete(user)}>
+                            Excluir
+                        </Button>
+                        </td>
+                    </tr>
+                ))
+
+            )}
+            </tbody>
+            </Table>            
+        </Container>
+        </>
+    )
+}
+
+
+
+
